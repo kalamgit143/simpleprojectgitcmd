23d78a43c8d95dba3f6c9d85188ecbb96f5418b3
kalamgit143 <magnitiait@gmail.com>
2023-08-13 13:25:40 +0530

    Some more changes

--- src/test/java/mypack/Test2.java
+++ src/test/java/mypack/Test2.java
@@ -1,9 +1,11 @@
 package mypack;
 
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Test2 
 {
 	RemoteWebDriver driver;
+	WebDriverWait wait;
 
 }
